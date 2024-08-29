9dec89c8c406870092556d0f045dc28016644f71
josegabo04 <jose.lda04@gmail.com>
2024-08-29 18:21:48 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'merakiMovil',
+  webDir: 'www'
+};
+
+export default config;
